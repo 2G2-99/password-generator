92a2cb040f9a76f1e81c99328de52dc69e0d5855
Santiago <sagos0919@gmail.com>
2022-11-18 18:34:26 +0000

    Starter code added

---
+++ starter/script.js
@@ -0,0 +1,135 @@
+/**
+ * PSEUDOCODE
+  - Create a function where a prompt gets a numeric input specifying how many characters the users want in their password (no less than 10 and no more than 64)
+
+    On that same function there should be multiple confirms, where the users decide what character type they want to include.
+
+  - With the validated character types (minimum one) generate a random string (It may begin as an array but then it gets concatenated)
+
+  - When the password has been generated, then it has to be displayed on the webpage
+ */
+
+// Array of special characters to be included in password
+var specialCharacters = [
+	'@',
+	'%',
+	'+',
+	'\\',
+	'/',
+	"'",
+	'!',
+	'#',
+	'$',
+	'^',
+	'?',
+	':',
+	',',
+	')',
+	'(',
+	'}',
+	'{',
+	']',
+	'[',
+	'~',
+	'-',
+	'_',
+	'.',
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+	let strLength = prompt(
+		'How many characters would you like to have on your password?',
+		10
+	);
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {}
+
+// Function to generate password with user input
+function generatePassword() {}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+	var password = generatePassword();
+	var passwordText = document.querySelector('#password');
+
+	passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+let charTypes = {
+	specialCharacters: specialCharacters,
+	numericCharacters: numericCharacters,
+	lowerCasedCharacters: lowerCasedCharacters,
+	upperCasedCharacters: upperCasedCharacters,
+};
